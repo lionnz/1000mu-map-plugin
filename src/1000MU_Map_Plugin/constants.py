d27ccfc600bc8206d366e839dfffca85b5412dfd
liuhao <1000mu04@liuhaodeiMac.local>
2026-07-15 17:00:06 +0800

    修复预设顺序：精确关键词优先，避免子串误匹配
    - 路名_主路/路名_支路 移到 主路/支路 前面
    - 商场边框 移到 商场 前面
    - 解决「商场」误匹配「商场边框」为 80m、以及「主路」误匹配「路名_主路」为 20m 的问题
    - 同步更新所有文档中的预设表

--- src/1000MU_Map_Plugin/constants.py
+++ src/1000MU_Map_Plugin/constants.py
@@ -3,15 +3,15 @@ import hashlib
 INVALID_LAYER_NAMES = {'svg','g','path','rect','circle','ellipse','polygon','polyline','line','layer','group','root','vector'}
 
 BUILTIN_HEIGHT_PRESETS = [
-    ("商场", 80.0),
+    ("路名_主路", 20.10),
+    ("路名_支路", 15.10),
     ("商场边框", 60.0),
+    ("商场", 80.0),
     ("深色box", 30.0),
     ("中色box", 40.0),
     ("浅色box", 50.0),
     ("主路", 20.0),
     ("支路", 15.0),
-    ("路名_主路", 20.10),
-    ("路名_支路", 15.10),
     ("绿化", 10.0),
     ("水", 5.0),
 ]
